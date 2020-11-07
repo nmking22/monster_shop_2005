45565816664a518276a08cc1abf9b68480b75130
Brett Turing <bjsherman80@gmail.com>
2020-11-07 13:21:54 -0700

    remove discount item model spec

--- spec/models/discount_item_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe DiscountItem, type: :model do
-    describe 'relationships' do
-    it {should belong_to :item}
-    it {should belong_to :discount}
-  end
-end
