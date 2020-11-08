60845d79cbec2671211b5b7258ffc5fceb5bb023
Brian Liu <brian.b.liu@gmail.com>
2020-11-07 20:59:34 -0700

    Create bulk discounts controller will CRUD actions.

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,4 +1,35 @@
 class Merchant::BulkDiscountsController < Merchant::BaseController
   def index
+    @bulk_discounts = BulkDiscount.all
+  end
+
+  def new
+  end
+
+  def create
+    @bulk_discount = BulkDiscount.new(discount_params)
+    @bulk_discount.save
+    redirect_to '/merchant/bulk_discounts'
+  end
+
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.update(discount_params)
+    redirect_to '/merchant/bulk_discounts'
+  end
+
+  def destroy
+    bulk_discount = BulkDiscount.find(params[:id])
+    bulk_discount.destroy!
+    redirect_to '/merchant/bulk_discounts'
+  end
+
+  private
+  def discount_params
+    params.permit(:description, :discount_percent, :minimum_quantity)
   end
 end
