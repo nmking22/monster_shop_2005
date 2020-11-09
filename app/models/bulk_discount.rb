50e289f7f1d55b56890eaae2920d71175a6b00de
Brian Liu <brian.b.liu@gmail.com>
2020-11-09 16:39:44 -0700

    Add method calculate discount and apply discount.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,4 +2,12 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :description, :discount_percent, :minimum_quantity
 
   belongs_to :merchant
+
+  def calculate_discount(total)
+    total * ((100 - discount_percent) * 0.01)
+  end
+
+  def self.apply_discount(quantity)
+    order(:minimum_quantity).where("minimum_quantity <= ?", quantity).last
+  end
 end
